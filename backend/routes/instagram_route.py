010a54a975d9023b5a8364e344070022369ce72d
Nikita Clark <nclark@junzitechsolutions.com>
2026-01-18 08:32:53 -0600

    Fix: Prioritize environment variables for Facebook App ID and Secret with correct fallbacks

--- backend/routes/instagram_route.py
+++ backend/routes/instagram_route.py
@@ -39,8 +39,8 @@ from zoneinfo import ZoneInfo
 
 load_dotenv()
 
-FB_APP_ID = os.getenv("FB_APP_ID")
-FB_APP_SECRET = os.getenv("FB_APP_SECRET")
+FB_APP_ID = os.getenv("FB_APP_ID", "25266816979636362")
+FB_APP_SECRET = os.getenv("FB_APP_SECRET", "bfc276034da82f0f60ac7112163d809a")
 REDIRECT_URI = os.getenv("REDIRECT_URI")
 GRAPH_API_URL = "https://graph.facebook.com/v23.0"
 
